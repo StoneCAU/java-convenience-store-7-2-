7fecd0e2abb107bd642c4661b6695b35866c3806
StoneCAU <seokwoo1999@naver.com>
2024-12-13 02:31:11 +0900

    feat: 현재 보유하고 있는 상품 출력

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,9 +1,13 @@
 package store.controller;
 
+import store.domain.Products;
+import store.util.FileLoader;
 import store.view.OutputView;
 
 public class StoreController {
     public void run() {
         OutputView.printWelcomeMessage();
+        Products products = FileLoader.getProducts();
+        OutputView.printProducts(products);
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,10 +1,18 @@
 package store.view;
 
+import store.domain.Products;
+
 public class OutputView {
     private static final String NEW_LINE = System.lineSeparator();
 
     public static void printWelcomeMessage() {
         System.out.println("안녕하세요. W편의점입니다.");
+    }
+
+    public static void printProducts(Products products) {
+        System.out.println("현재 보유하고 있는 상품입니다.");
+        printNewLine();
+        products.getProducts().forEach(System.out::println);
     }
 
     public static void printErrorMessage(String message) {
